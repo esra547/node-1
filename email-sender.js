d1eec08f4585f46d63b9e23a5802c179ca72525c
esra547 <esra.bouallegui@gmail.com>
2025-07-17 12:36:31 +0100

    send

--- email-sender.js
+++ email-sender.js
@@ -7,8 +7,8 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
   }
-});
-
+})
+     
 const mailOptions = {
   from: process.env.EMAIL_USER,
   to: 'esra.bouallegui@gmail.com',
